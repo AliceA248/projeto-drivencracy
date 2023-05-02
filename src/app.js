8ee1a149c82b79d3ec526dc49045682939382bbd
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2023-05-02 19:14:03 -0300

    setup inicial

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express"
+import cors from "cors"
+
+
+// Criação do servidor
+const app = express()
+
+// Configurações
+app.use(express.json())
+app.use(cors())
+
+
+// Deixa o app escutando, à espera de requisições
+const PORT = 5002
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
