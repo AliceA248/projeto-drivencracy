f9147a284c7d315af2c00c17306895199749def3
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2023-05-05 17:03:35 -0300

    shemas importados corretamente

--- src/schemas/schema.js
+++ src/schemas/schema.js
@@ -1,16 +1,13 @@
 import joi from "joi";
 
-const choiceSchema = joi.object({
+export const choiceSchema = joi.object({
   title: joi.string().required(),
   poolId: joi.string().required(),
 });
 
-const poolSchema = joi.object({
+export const poolSchema = joi.object({
   title: joi.string().required(),
   expireAt: joi.date(),
 });
 
-
-
-export default { poolSchema, choiceSchema };
 
