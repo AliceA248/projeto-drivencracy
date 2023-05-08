2cd5ee5c6e19c24ff34caeddab01976151d6baa8
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2023-05-08 12:03:50 -0300

    ajustes finais

--- src/schemas/schema.js
+++ src/schemas/schema.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 export const choiceSchema = joi.object({
   title: joi.string().required(),
-  poolId: joi.string().required(),
+  pollId: joi.string().required(),
 });
 
 export const pollSchema = joi.object({
